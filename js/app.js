e1976b78c762b687fcb861a50fb1c2dd546cb003
Ruth Gendro <ruthjg90@gmail.com>
2016-10-21 14:49:57 -0700

    added foursquare and ajaxrequest

--- js/app.js
+++ js/app.js
@@ -94,4 +94,85 @@ var ViewModel = function () {
     // Initialize marker
     var marker;
 
+        // For each place, set markers, request Foursquare data, and set event listeners for the infowindow
+    // Credit https://github.com/kacymckibben/project-5-app.git
+    self.placeList().forEach(function (placeItem) {
+
+        // Define markers for each place
+        marker = new google.maps.Marker({
+            position: new google.maps.LatLng(placeItem.lat(), placeItem.lng()),
+            map: map,
+            animation: google.maps.Animation.DROP
+        });
+        placeItem.marker = marker;
+
+        // Make AJAX request to Foursquare
+        $.ajax({
+            url: 'https://api.foursquare.com/v2/venues/' + placeItem.id() +
+            '?client_id=NONGGLXBKX5VFFIKKEK1HXQPFAFVMEBTRXBWJUPEN4K14JUE&client_secret=ZZDD1SLJ4PA2X4AJ4V23OOZ53UM4SFZX0KORGWP5TZDK4YYJ&v=20130815',
+            dataType: "json",
+            success: function (data) {
+                // Make results easier to handle
+                var result = data.response.venue;
+
+                // placeItem.name(result.name);
+
+                // The following lines handle inconsistent results from Foursquare
+                // Check each result for properties, if the property exists,
+                // add it to the Place constructor
+
+
+                var location = result.hasOwnProperty('location') ? result.location : '';
+                if (location.hasOwnProperty('address')) {
+                    placeItem.address(location.address || '');
+                }
+
+                var bestPhoto = result.hasOwnProperty('bestPhoto') ? result.bestPhoto : '';
+                if (bestPhoto.hasOwnProperty('prefix')) {
+                    placeItem.photoPrefix(bestPhoto.prefix || '');
+                }
+
+                if (bestPhoto.hasOwnProperty('suffix')) {
+                    placeItem.photoSuffix(bestPhoto.suffix || '');
+                }
+
+                var description = result.hasOwnProperty('description') ? result.description : '';
+                placeItem.description(description || '');
+
+                var url = result.hasOwnProperty('url') ? result.url : '';
+                placeItem.url(url || '');
+
+                placeItem.canonicalUrl(result.canonicalUrl);
+
+                // Infowindow code is in the success function so that the error message
+                // displayed in infowindow works properly, instead of a mangled infowindow
+                // Credit https://discussions.udacity.com/t/trouble-with-infowindows-and-contentstring/39853/14
+
+                // Content of the infowindow
+                var contentString = '<div id="iWindow"><h4>' + placeItem.name() + '</h4><div id="pic"><img src="' +
+                        placeItem.photoPrefix() + '110x110' + placeItem.photoSuffix() +
+                        '" alt="Image Location"></div><p>Information from Foursquare:</p><p>' +
+                        placeItem.phone() + '</p><p>' + placeItem.address() + '</p><p>' +
+                        placeItem.description() + '</p><p>Rating: ' + placeItem.rating() +
+                        '</p><p><a href=' + placeItem.url() + '>' + placeItem.url() +
+                        '</a></p><p><a target="_blank" href=' + placeItem.canonicalUrl() +
+                        '>Foursquare Page</a></p><p><a target="_blank" href=https://www.google.com/maps/dir/Current+Location/' +
+                        placeItem.lat() + ',' + placeItem.lng() + '>Directions</a></p></div>';
+
+                // Add infowindows credit http://you.arenot.me/2010/06/29/google-maps-api-v3-0-multiple-markers-multiple-infowindows/
+                google.maps.event.addListener(placeItem.marker, 'click', function () {
+                    infowindow.open(map, this);
+                    // Bounce animation credit https://github.com/Pooja0131/FEND-Neighbourhood-Project5a/blob/master/js/app.js
+                    placeItem.marker.setAnimation(google.maps.Animation.BOUNCE);
+                    setTimeout(function () {
+                        placeItem.marker.setAnimation(null);
+                    }, 500);
+                    infowindow.setContent(contentString);
+                });
+            },
+
+
+    });
+
+
 };
