c01cc0b1f288f83573bcc748b888ffd220999463
Ruth Gendro <ruthjg90@gmail.com>
2016-10-21 14:23:33 -0700

    added app.js file with data

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// Array of locations
+var locations = [
+    {
+        name: "Lake Margaret",
+        lat: 47.769702,
+        lng: -121.901639,
+        // id: "" ?
+    },
+    {
+        name: "Lake Shannon",
+        lat: 48.561961,
+        lng: -121.87395,
+        // id: "" ?
+    },
+    {
+        name: "Cle Elum Lake",
+        lat: 47.279637,
+        lng: -121.10405,
+        // id: "" ?
+    },
+    {
+        name: "Lake Wenatchee",
+        lat: 47.824453,
+        lng: lng: -120.777017,
+        // id: "" ?
+    },
+    {
+        name: "Big Lake",
+        lat: 48.402883,
+        lng: lng: -122.241265,
+        // id: "" ?
+    }
+];
